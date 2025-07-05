7825455122fdf5e4feed09c57b3e2a010321f7df
Klauswk <17770979+Klauswk@users.noreply.github.com>
2025-07-05 13:12:01 +0200

    Build without specifying module

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-$JAVA_HOME/bin/javac -Xlint:deprecation -cp "libs/*" --add-exports jdk.jdi/com.sun.tools.example.debug.expr=ALL-UNNAMED -d target/ ./src/*.java
+$JAVA_HOME/bin/javac -Xlint:deprecation --module-path libs -cp "libs/*" -d target/ ./src/*.java
---
+++ src/module-info.java
@@ -0,0 +1,6 @@
+module com.github.klauswk.tty { 
+  requires org.jline.reader;
+  requires org.jline.terminal;
+  requires transitive jdk.jdi;
+}
+
